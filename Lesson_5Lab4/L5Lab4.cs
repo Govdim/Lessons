9a4adeff999197a477c3efb850aaf52f7aa89751
Govdim <govorek1234511@gmail.com>
2021-12-01 14:09:52 +0300

    Lesson 5 Lab 4

---
+++ Lesson_5Lab4/L5Lab4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson_5Lab4{
+    
+    public class L5Lab4{
+
+        static void Main(string[] args){
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            
+            MinmaxSeq(ref min, ref max);
+            Console.WriteLine("минимальное: " + min + ", максимальное: " + max);
+        }
+
+        static void MinmaxSeq(ref int min, ref int max){
+            Console.Write("Введите полседовательность и закончите ввод нулем: ");
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(" "), i => int.Parse(i));
+
+            for(int i = 0; i < numbers.Length; i++){
+                int num = numbers[i];
+                if(num != 0){
+                    if(num < min){
+                        min = num;
+                    }
+
+                    if(num > max){
+                        max = num;
+                    }
+                }
+            }
+        }
+    }
+}
