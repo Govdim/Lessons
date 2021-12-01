c95a656674ba58a3623863d40d068a0c18a89f40
Govdim <govorek1234511@gmail.com>
2021-12-01 13:27:08 +0300

    Lesson 5 Task 1

---
+++ Lesson_5Task1/L5task1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_5Task1{
+    public class L5Task1{
+
+        static void Main(string[] args){
+            Console.Write("Введите 3 стороны треугольника через пробел: ");
+            int[] sides = Array.ConvertAll(Console.ReadLine().Split(" "), i => int.Parse(i));
+            Perimeter(sides[0], sides[1], sides[2]);
+
+            Console.ReadLine();
+        }
+
+        static void Perimeter(int side1, int side2, int side3){
+            int perimeter = side1 + side2 + side3;
+        }
+    }
+}
